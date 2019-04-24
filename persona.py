a7c32749c4fc107548f2a00ea188dea27396bd24
Ernestina Plate <ernestina.plate@gmail.com>
2019-04-24 17:50:49 -0400

    Se aclararon los METODOS en dino.py y se modifico la clase Persona en persona.py

--- persona.py
+++ persona.py
@@ -1,10 +1,19 @@
 # Creat una clase Persona con atributo nombre
 # Despues instanciar un objeto de tipo persona
 
+# Modificar la clase persona, agregarle un atributo edad
+# y un metodo cumple_anos
+
 class Persona:
     nombre = None
-    def __init__(self, un_nombre):
+    edad = None
+    def __init__(self, un_nombre, una_edad):
         self.nombre = un_nombre
-        print("Mi nombre es " + str(self.nombre) + "!\n")
+        self.edad = una_edad
+        print("Mi nombre es " + str(self.nombre) + " y tengo " + str(self.edad) + " anos!\n")
+    def set_nombre(self, nuevo_nombre):
+        self.nombre = nuevo_nombre
+    def cumple_anos(self):
+        self.edad += 1
 
-nina = Persona("Ernestina")
+nina = Persona("Ernestina", 19)
